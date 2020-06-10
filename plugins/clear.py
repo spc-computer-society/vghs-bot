8ad1f9582e97f25d24e9c5296984f59247947e88
Hoyin7123 <hoyin.wong712@gmail.com>
2020-06-10 20:15:17 +0800

    bug fix

--- plugins/clear.py
+++ plugins/clear.py
@@ -6,13 +6,20 @@ class Clear(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, limit: int = 5):
         await ctx.channel.purge(limit=limit + 1)
-        await ctx.send("Bye!",delete_after=2)
+        await ctx.send("Bye!", delete_after=2)
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, err):
-        await ctx.send("An error occurred while trying to do that!")
+    @clear.error
+    async def clear_error(self, ctx, err):
+        if isinstance(err, commands.BadArgument):
+            await ctx.send("Limit must be an integer!")
+        elif isinstance(err, commands.MissingPermissions):
+            await ctx.send("Sorry, permission denied!")
+        else:
+            await ctx.send("An error occurred while trying to do that!\n"
+                           f"{err}")
 
 
 def setup(bot):
